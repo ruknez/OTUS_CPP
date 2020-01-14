84c76658d98e065285281126eab72d4005ceb639
Ruknez <ruknez94@mail.ru>
2020-01-15 00:09:37 +0300

    modif test

--- 1/test_version.cpp
+++ 1/test_version.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_MODULE test_version
 
 #include "lib.h"
+#include "version.h"
 
 #include <boost/test/unit_test.hpp>
 
@@ -8,7 +9,7 @@ BOOST_AUTO_TEST_SUITE(test_version)
 
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
-    BOOST_CHECK(version() > 0);
+    BOOST_CHECK(version() == PROJECT_VERSION_PATCH);
 }
 
 }
